1486048c3f10087e761ad6b3ea3b65cc913c433f
Aditya <adarya125@gmail.com>
2025-05-25 11:25:20 +0530

    Project Structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aditya Arya",
+    author_email="adarya125@gmail.com",
+    packages=find_packages()
+)
